6f3371844de7d2925fe05f452d9b63dccfe74c30
Yutaka HARA <yutaka.hara+github@gmail.com>
2009-08-02 18:18:51 +0900

    fix: use Module#included

--- lib/item.rb
+++ lib/item.rb
@@ -1,13 +1,15 @@
 module Item
-  belongs_to :account
-  belongs_to :category
+  def self.included(klass)
+    klass.belongs_to :account
+    klass.belongs_to :category
 
-  validates_presence_of :title, :date
+    klass.validates_presence_of :title, :date
 
-  validates_presence_of :amount, :index_of_day
-  validates_numericality_of :amount, :index_of_day
+    klass.validates_presence_of :amount, :index_of_day
+    klass.validates_numericality_of :amount, :index_of_day
 
-  validates_presence_of :account
+    klass.validates_presence_of :account
+  end
 
   def validate_category
     unless self.category && self.category.kind == self.class.to_s
