3d32a0d3bcf04579283e395b480a571efcc2a444
Yutaka HARA <yutaka.hara+github@gmail.com>
2009-08-02 17:06:32 +0900

    Created table for income, expense, move

---
+++ db/migrate/20090802075613_create_incomes.rb
@@ -0,0 +1,17 @@
+class CreateIncomes < ActiveRecord::Migration
+  def self.up
+    create_table :incomes do |t|
+      t.string :title
+      t.integer :amount
+      t.integer :categoly_id
+      t.date :date
+      t.integer :index_of_day
+
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :incomes
+  end
+end
