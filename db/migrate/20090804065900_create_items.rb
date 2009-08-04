b8e805e6557462240356df059e1491586cf592ca
Yutaka HARA <yutaka.hara+github@gmail.com>
2009-08-04 16:03:05 +0900

    Jointed 3 migrations to items

---
+++ db/migrate/20090804065900_create_items.rb
@@ -0,0 +1,26 @@
+class CreateItems < ActiveRecord::Migration
+  def self.up
+    create_table :items do |t|
+      # common attributes
+      t.string :title
+      t.integer :amount
+      t.integer :category_id
+      t.date :date
+      t.integer :index_of_day
+      t.timestamps
+
+      # for Expense, Income
+      t.integer :account_id
+      # for Move
+      t.integer :account_id_from
+      t.integer :account_id_to
+
+      # for STI
+      t.string :type 
+    end
+  end
+
+  def self.down
+    drop_table :items
+  end
+end
