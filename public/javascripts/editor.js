bfedd1db267fdb2312c10340d6b76d33d975c465
Yutaka HARA <yutaka.hara+github@gmail.com>
2009-08-04 14:03:27 +0900

    Insert_row button

--- public/javascripts/editor.js
+++ public/javascripts/editor.js
@@ -1,5 +1,23 @@
 var $j = jQuery.noConflict();
 
+var MoneyRail = MoneyRail || {};
+
+MoneyRail.insert_row = function(n){
+  var make_tds = function(k){
+    return $R(1, k).map(function(i){
+      return "<td></td><td></td>";
+    }).join("");
+  };
+
+  $j("#row"+n).after([
+    "<tr>",
+    "<td>▽</td>",
+    "<td></td>", // Date
+    MoneyRail.category_numbers.map(make_tds).join(""),
+    "</tr>"
+  ].join(""));
+};
+
 $j(function(){
-  alert("hi");
+  //alert("hi");
 });
