83d982b404897a3a55470a961dc9829dff79e985
Yutaka HARA <yutaka.hara+github@gmail.com>
2009-08-05 10:30:20 +0900

    Compress rows into days

--- app/controllers/logs_controller.rb
+++ app/controllers/logs_controller.rb
@@ -2,10 +2,10 @@ class LogsController < ApplicationController
 
   def edit
     @year, @month = params[:year].to_i, params[:month].to_i
-    @month_range = Range.new(
-      Date.new(@year, @month).beginning_of_month,
-      Date.new(@year, @month).end_of_month
-    )
+
+    first_day = Date.new(@year, @month)
+    @month_range = first_day .. ((first_day >> 1) - 1)
+
     @accounts = Account.find(:all)
     @categories = Category.hashed
   end
--- app/helpers/logs_helper.rb
+++ app/helpers/logs_helper.rb
@@ -6,20 +6,29 @@ module LogsHelper
 
     condition = {
       :conditions => {:date => @month_range},
-      :order => "date, index_of_day",
+      #:order => "date, index_of_day",
     }
     items = [
       account.expenses.all(condition),
       account.incomes.all(condition),
       account.moves_from.all(condition),
       account.moves_to.all(condition),
-    ].flatten
+    ].flatten(1)
 
-    items.map{|item|
-      cat_all.map{|cat|
-        cat == item.category ? item : nil
-      }.unshift(item.date)
-    }
+    @month_range.map{|today|
+      todays, items = items.partition{|m| m.date == today}
+      if todays.empty?
+        nil
+      else
+        max_index = todays.map(&:index_of_day).max
+
+        (0..max_index).map{|i|
+          cat_all.map{|cat|
+            todays.find{|m| m.category == cat && m.index_of_day == i}
+          }.unshift(today)
+        }
+      end
+    }.compact.flatten(1)
   end
 
 end
