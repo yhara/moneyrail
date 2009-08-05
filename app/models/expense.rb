68fd4ca741e5a52a33fca6ac750f49457682cc44
Yutaka HARA <yutaka.hara+github@gmail.com>
2009-08-05 15:40:52 +0900

    Extracted ancestral class SimpleItem

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -1,26 +1,2 @@
-class Expense < Item
-  # associations
-  
-  belongs_to :account
-  
-  # validations
-
-  validates_presence_of :account
-
-  def find_conflict
-    Expense.first(:conditions => {
-      :date => date, 
-      :account_id => account_id,
-      :category_id => category_id,
-      :position => position,
-    })
-  end
-  
-  # featues
-
-  acts_as_list :scope => [
-    'date => #{date}',
-    'account_id => #{account_id}',
-    'category_id => #{category_id}',
-  ].join(" AND ")
+class Expense < SimpleItem
 end
--- app/models/income.rb
+++ app/models/income.rb
@@ -1,26 +1,2 @@
-class Income < Item
-  # associations
-  
-  belongs_to :account
-  
-  # validations
-
-  validates_presence_of :account
-
-  def find_conflict
-    Income.first(:conditions => {
-      :date => date, 
-      :account_id => account_id,
-      :category_id => category_id,
-      :position => position,
-    })
-  end
-
-  # featues
-
-  acts_as_list :scope => [
-    'date => #{date}',
-    'account_id => #{account_id}',
-    'category_id => #{category_id}',
-  ].join(" AND ")
+class Income < SimpleItem
 end
