cce5ec6ff7a97cd1c7ee02d3fde4ca8bef8ac9fe
Yutaka HARA <yutaka.hara+github@gmail.com>
2009-08-07 18:23:49 +0900

    find account, date, position from html

--- public/javascripts/editor.js
+++ public/javascripts/editor.js
@@ -2,6 +2,27 @@ var $j = jQuery.noConflict();
 
 var MoneyRail = MoneyRail || {};
 
+MoneyRail.raise = function(){
+  $j.each(arguments, console.error);
+};
+
+// refrection
+MoneyRail.get_acount_id = function(input_elem){
+  var table_elem = $j(input_elem).parents("table");
+  if(table_elem.length == 0){
+    MoneyRail.raise("table not found:", input_elem);
+  }
+  return $j(table_elem).attr("title");
+};
+MoneyRail.get_date_and_position = function(input_elem){
+  var tr_elem = $j(input_elem).parents("tr");
+  if(tr_elem.length == 0){
+    MoneyRail.raise("tr not found:", input_elem);
+  }
+  var embeded = $j(tr_elem).attr("title");
+  return embeded.split(/_/);
+};
+
 MoneyRail.insert_row = function(elem){
   var make_tds = function(k){
     return $R(1, k).map(function(i){
@@ -12,11 +33,12 @@ MoneyRail.insert_row = function(elem){
     });
   };
 
+  var a = MoneyRail.get_date_and_position(elem);
+  var date = a[0], position = Number(a[1]);
   var row = $j(elem).parents("tr");
-  var date_str = row.attr("title");
 
   $j(row).after([
-    "<tr title='", date_str, "'>",
+    "<tr title='", date, "_", (position+1), "'>",
       // Button
       "<td class='inserts'>",
         "<span class='pushable'>",
