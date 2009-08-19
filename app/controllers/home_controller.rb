22744978052a997751b3f4eb6c3c012abc497f1c
Yutaka HARA <yutaka.hara+github@gmail.com>
2009-08-19 15:23:09 +0900

    Added links to months

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,2 +1,31 @@
 class HomeController < ApplicationController
+
+  def index
+    @months = []
+    collect_months(@months)
+    append_current_month(@months)
+  end
+
+  private
+
+  def collect_months(months)
+    oldest = Item.minimum("date")
+    newest = Item.maximum("date")
+
+    if oldest and newest
+      m = newest.beginning_of_month
+      while m >= oldest.beginning_of_month
+        months.push m
+        m <<= 1
+      end
+    end
+  end
+
+  def append_current_month(months)
+    current = Time.now.to_date.beginning_of_month
+    unless months.include?(current)
+      months.push current
+    end
+  end
+
 end
