b2ca1746cf914429f751ca9187c97b5902d34021
Yutaka HARA <yutaka.hara+github@gmail.com>
2009-09-30 17:43:50 +0900

    fix #12: Show link to neighbor months

--- app/controllers/logs_controller.rb
+++ app/controllers/logs_controller.rb
@@ -1,12 +1,18 @@
 class LogsController < ApplicationController
 
   def view
+    # param
     @mode = params[:mode]
     @year, @month = params[:year].to_i, params[:month].to_i
 
+    # date
     first_day = Date.new(@year, @month)
     @month_range = first_day .. ((first_day >> 1) - 1)
+    @current_month = first_day
+    @prev_month = first_day << 1
+    @next_month = first_day >> 1
 
+    # items
     @accounts = Account.all(:order => "position")
     @categories = Category.hashed
     @cat_all = [
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,33 @@ module ApplicationHelper
   def format_date(d)
     d.strftime("%Y-%m-%d (%a)")
   end
+
+  def format_month(d)
+    d.strftime("%Y-%m")
+  end
+
+  # (str, date) or (date)
+  def link_to_edit_logs(a, b=nil)
+    str, date = str_and_date(a, b)
+
+    link_to str, edit_logs_path(:year => date.year, :month => date.month)
+  end
+
+  def link_to_show_logs(a, b=nil)
+    str, date = str_and_date(a, b)
+
+    link_to str, show_logs_path(:year => date.year, :month => date.month)
+  end
+
+  private
+
+  def str_and_date(a, b)
+    if b 
+      [a, b]
+    else
+      [format_month(a), a]
+    end
+  end
+
+
 end
