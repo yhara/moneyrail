17fa34af3871831f3129870b17bd265891741ea5
Yutaka HARA <yutaka.hara+github@gmail.com>
2009-11-01 22:49:17 +0900

    Stop using RubyStation::Helper::Rails
    because RubyStation::Helper::Rails is removed in RubyStation 0.1.5
    modify config/database.yml and load script/server

--- main.rb
+++ main.rb
@@ -2,4 +2,16 @@
 require 'optparse'
 require 'ruby-station'; RubyStation.parse_argv
 
-RubyStation::Helper::Rails.run
+# setup database
+unless File.exist?(RubyStation.data_path("production.sqlite3"))
+  yml_path = File.join(File.dirname(__FILE__), "config/database.yml")
+  conf = YAML.load_file(yml_path)
+  conf["production"]["database"] = RubyStation.data_path("production.sqlite3")
+  File.open(yml_path, "w"){|f| YAML.dump(conf, f) }
+
+  system "rake db:migrate db:seed RAILS_ENV=production"
+end
+
+ARGV.push "--port=#{RubyStation.port}"
+ARGV.push "--environment=production"
+load File.join(File.dirname(__FILE__), 'script/server')
